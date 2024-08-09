82cace88fca0b5c038589c2f19b276a77b09551d
coderAung <yewontaung798@gmail.com>
2024-08-09 16:42:54 +0630

    add some sample codes

--- social-app/src/main/java/edu/ucsy/social/controller/HomeController.java
+++ social-app/src/main/java/edu/ucsy/social/controller/HomeController.java
@@ -1,7 +1,20 @@
 package edu.ucsy.social.controller;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet({"/home"})
 public class HomeController extends Controller {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		view(req, resp, "home");
+	}
 
 }
