4e2debcbe92c6c4b0e924337b609bce680e2c771
Skyllar1 <skyllar64@gmail.com>
2024-08-11 21:35:49 +0630

    commentModel update

--- social-app/src/main/java/edu/ucsy/social/model/CommentModel.java
+++ social-app/src/main/java/edu/ucsy/social/model/CommentModel.java
@@ -55,8 +55,15 @@ public class CommentModel extends  AbstractModel <Comment>{
 	}
 	
 	private Comment commentFrom(ResultSet rs) throws SQLException {
-		
-		return null;
+		var comment = new Comment(
+				rs.getLong(1),
+				rs.getString(2), 
+				rs.getTimestamp(3).toLocalDateTime(),
+				rs.getTimestamp(4).toLocalDateTime(),
+				rs.getLong(5),
+				rs.getString(6),
+				rs.getLong(7));
+		return comment;
 	}
 
 	@Override
