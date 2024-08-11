05f9cfaca4d7d865223ef981292212acb24a1126
Jue Jue San <sanjuejue79@gmail.com>
2024-08-11 15:18:38 +0630

    updating url testing prototype

--- social-app/src/main/java/edu/ucsy/social/controller/PostController.java
+++ social-app/src/main/java/edu/ucsy/social/controller/PostController.java
@@ -7,15 +7,23 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet({"/profile/edit"})
-public class ProfileEditController extends Controller{
+@WebServlet({("/post/*")})
+public class PostController extends Controller{
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		view(req, resp, "profile/edit");
-	}
+		String action = req.getPathInfo();
+		
+		if(action.equals("/create")) {
+			view(req, resp, "post/create");
+		}else if(action.equals("/detail")) {
+			view(req, resp, "post/detail");
+		}else {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 
+		}
+	}
 }
--- social-app/src/main/java/edu/ucsy/social/controller/ProfileController.java
+++ social-app/src/main/java/edu/ucsy/social/controller/ProfileController.java
@@ -6,15 +6,35 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-@WebServlet({"/profile"})
-public class ProfileController extends Controller{
 
-	private static final long serialVersionUID = 1L;
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		view(req, resp, "profile");
-	}
+@WebServlet({"/profile/*"})
+public class ProfileController extends Controller {
 
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Extract the path after "/profile/"
+        String action = req.getPathInfo();
+
+        if (action == null || action.equals("/profile")) {
+           
+            view(req, resp, "profile");
+        } else {
+            switch (action) {
+                case "/detail":
+                    view(req, resp, "profile/detail");
+                    break;
+                case "/edit":
+                    view(req, resp, "profile/edit");
+                    break;
+                case "/friends":
+                    view(req, resp, "profile/friends");
+                    break;
+                default:
+                    resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                    break;
+            }
+        }
+    }
 }
