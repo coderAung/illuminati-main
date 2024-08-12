0355cfb3cdface2a98fcd01286696d83a16a6975
coderAung <yewontaung798@gmail.com>
2024-08-12 20:27:00 +0630

    updated Post

---
+++ social-app/src/main/java/edu/ucsy/social/model/entity/Post.java
@@ -0,0 +1,26 @@
+package edu.ucsy.social.model.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record Post(
+		long id,
+		String content,
+		List<PostImage> postImages,
+		LocalDateTime createdAt,
+		LocalDateTime updatedAt,
+		long userId,
+		String userName) {
+
+	public Post(String content, List<PostImage> postImages, long userId, String userName) {
+		this(0, content, postImages, null, null, userId, userName);
+	}
+	
+	public Post perfectClone(long id, List<PostImage> postImages, LocalDateTime createdAt, LocalDateTime updatedAt) {
+		return new Post(id, content, postImages, createdAt, updatedAt, userId, userName);
+	}
+	
+	public Post updatedClone(LocalDateTime updatedAt) {
+		return new Post(id, content, postImages, createdAt, updatedAt, userId, userName);
+	}
+}
