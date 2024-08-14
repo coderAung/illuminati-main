98709351177d94cf0f572f1570000417ce08d11a
coderAung <yewontaung798@gmail.com>
2024-08-14 16:20:24 +0630

    updated sql and UserDetail entity

--- social-app/src/main/java/edu/ucsy/social/model/entity/UserDetail.java
+++ social-app/src/main/java/edu/ucsy/social/model/entity/UserDetail.java
@@ -5,6 +5,41 @@ import java.time.LocalDate;
 public record UserDetail(
 		long userid,
 		LocalDate birthDate,
-		String address) {
+		String address,
+		String bio,
+		String phoneNumber,
+		Gender gender,
+		Relationship relationship,
+		Occupation occupation) {
 
+	public enum Relationship {
+		Single("Single"), 
+		Married("Married"), 
+		Divorced("Divorced"), 
+		InARelationship("In A Relationship"),
+		IsComplicated("Is Complicated");
+		
+		private String message;
+		
+		private Relationship(String message) {
+			this.message = message;
+		}
+		
+		public String getMessage() {
+			return this.message;
+		}
+	}
+	
+	public enum Occupation {
+		Student, 
+		Actor,
+		Singer, 
+		Dancer,
+		Influencer,
+		DigitalCreator
+	}
+	
+	public enum Gender {
+		Male, Female, Others
+	}
 }
