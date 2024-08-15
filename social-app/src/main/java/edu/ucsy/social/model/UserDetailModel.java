7b05678751d8cb94393d20fbbc53101191d1819f
Skyllar1 <skyllar64@gmail.com>
2024-08-15 22:57:58 +0630

    test 1

--- social-app/src/main/java/edu/ucsy/social/model/UserDetailModel.java
+++ social-app/src/main/java/edu/ucsy/social/model/UserDetailModel.java
@@ -2,7 +2,10 @@ package edu.ucsy.social.model;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.mysql.cj.protocol.Resultset;
@@ -18,77 +21,128 @@ public class UserDetailModel extends AbstractModel<UserDetail>{
 
 	public UserDetailModel(DatabaseConnector connector) {
 		super(connector);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public UserDetail save(UserDetail t) {
-		// TODO Auto-generated method stub
+	public UserDetail save(UserDetail ud) {
+		var sql = """
+				insert into user_details (birth_date, address, bio, phone_number, gender, relationship, occupation)
+				 values(?, ?, ?, ?, ?, ?, ?)
+				""";
+		
+		try(var conn = connector.getConnection();
+				var stmt = conn.prepareStatement(sql)
+				){
+			
+			var birthDate = Timestamp.valueOf(LocalDateTime.now());
+			
+			if(!birthDate.equals(null)) {
+				stmt.setTimestamp(1, birthDate);
+			}else {
+				return null;
+			}
+			
+			if(!ud.address().equals(null)) {
+				stmt.setString(2, ud.address());
+			}else {
+				return null;
+			}
+			
+			if(!ud.bio().equals(null)) {
+				stmt.setString(3, ud.bio());
+			}else {
+				return null;
+			}
+			
+			if(!ud.phoneNumber().equals(null)) {
+				stmt.setString(4, ud.phoneNumber());
+			}else {
+				return null;
+			}
+			
+			if(!ud.gender().equals(null)) {
+				stmt.setInt(5, ud.gender().ordinal() + 1);
+			}else {
+				return null;
+			}
+			
+			if(!ud.relationship().equals(null)) {
+				stmt.setInt(6, ud.relationship().ordinal() + 1);
+			}else {
+				return null;
+			}
+			
+			if(!ud.occupation().equals(null)) {
+				stmt.setInt(7, ud.occupation().ordinal() + 1);
+			}else {
+				return null;
+			}
+			
+			var row = stmt.executeUpdate();
+			if(row == 0 ) {
+				return null;
+			}
+			
+			var keys = stmt.getGeneratedKeys();
+			if(keys.next()) {
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+				
+					
 		return null;
 	}
 
 	@Override
 	public UserDetail findOne(long id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ResultSet findOne(long id, String... cols) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<UserDetail> getAll() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<UserDetail> get(long limit) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public UserDetail update(UserDetail t) {
-		// TODO Auto-generated method stub
+	public UserDetail update(UserDetail ud) {
 		return null;
 	}
 
 	@Override
-	public UserDetail fullUpdate(UserDetail t) {
-		// TODO Auto-generated method stub
+	public UserDetail fullUpdate(UserDetail ud) {
 		return null;
 	}
 
 	@Override
 	public boolean delete(long id) {
-		// TODO Auto-generated method stub
+		var sql = "delete from user_details where id = ?";
+		try (var conn = connector.getConnection();
+				var stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+			stmt.setLong(1, id);
+			var row = stmt.executeUpdate();
+			if (row == 0) {
+				return false;
+			}
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 	
-//	long userid,
-//	LocalDate birthDate,
-//	String address,
-//	String bio,
-//	String phoneNumber,
-//	Gender gender,
-//	Relationship relationship,
-//	Occupation occupation
-
 	
-	private UserDetail userdetailFrom(ResultSet rs) throws SQLException{
-		var user = new UserDetail(
-				rs.getTimestamp("birth_date").toLocalDateTime(),
-				rs.getString("address"),
-				rs.getString("bio"),
-				rs.getString("phone_number"),
-				Gender.valueOf(rs.getString("gender")),
-				Relationship.valueOf(rs.getString("relationship")),
-				Occupation.valueOf(rs.getString("occupaton"))
-				);
-	}
 
 }
