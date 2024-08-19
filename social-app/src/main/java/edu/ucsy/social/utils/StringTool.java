862ac52a0c7f93cf4243aa6d4ce2b930f042f5e4
coderAung <yewontaung798@gmail.com>
2024-08-19 12:50:30 +0630

    added generateImageName method

--- social-app/src/main/java/edu/ucsy/social/utils/StringTool.java
+++ social-app/src/main/java/edu/ucsy/social/utils/StringTool.java
@@ -1,5 +1,9 @@
 package edu.ucsy.social.utils;
 
+import java.time.LocalDateTime;
+
+import edu.ucsy.social.model.entity.type.ImageStatus;
+
 public class StringTool {
 
 	public static String joinWithComma(String [] array) {
@@ -19,5 +23,22 @@ public class StringTool {
 				"".equals(string) || 
 				string.isEmpty() || 
 				string.isBlank();
+	}
+	
+	public static String generateImageName(String imageName, int userId, String userName) {
+		
+		var format = "%s_%s_%s_%s";
+		var dateString = LocalDateTime.now().toString();
+		return format.formatted(userId, userName, dateString, imageName);
+	}
+	
+	public static void main(String[] args) {
+		var name1 = generateImageName("img1.jsp", 1, "Aung Aung");
+		System.out.println(name1);
+
+		var name2 = generateImageName("img1.jsp", 1, "Aung Aung");
+		System.out.println(name2);
+		
+		System.out.println(name1.equals(name2));
 	}
 }
