4b1754e1899f803486fd6aa3c18b91a58df6d0c8
coderAung <yewontaung798@gmail.com>
2024-08-21 17:23:58 +0630

    merge with api dev

--- social-app/src/main/java/edu/ucsy/social/service/ServiceFactory.java
+++ social-app/src/main/java/edu/ucsy/social/service/ServiceFactory.java
@@ -10,7 +10,7 @@ public abstract class ServiceFactory {
 	public static <T> T getService(Class<T> t, DataSource dataSource) {
 		
 		if(t.equals(PostService.class)) {
-			
+
 		}
 		
 		if(t.equals(CommentService.class)) {
