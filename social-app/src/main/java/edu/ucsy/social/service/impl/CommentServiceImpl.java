d3d8a21bc41be01ae971e8548fec03318d156318
Skyllar1 <skyllar64@gmail.com>
2024-08-22 22:59:10 +0630

    test

--- social-app/src/main/java/edu/ucsy/social/service/impl/CommentServiceImpl.java
+++ social-app/src/main/java/edu/ucsy/social/service/impl/CommentServiceImpl.java
@@ -1,15 +1,18 @@
 package edu.ucsy.social.service.impl;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import edu.ucsy.social.data.Model;
 import edu.ucsy.social.data.ModelFactory;
+import edu.ucsy.social.data.OneToOne;
 import edu.ucsy.social.data.db.ConnectorFactory;
 import edu.ucsy.social.model.dto.form.CommentForm;
 import edu.ucsy.social.model.dto.view.CommentView;
 import edu.ucsy.social.model.entity.Comment;
+import edu.ucsy.social.model.entity.ProfileImage;
 import edu.ucsy.social.service.CommentService;
 
 public class CommentServiceImpl implements CommentService{
@@ -23,24 +26,36 @@ public class CommentServiceImpl implements CommentService{
 	@Override
 	public CommentView createComment(CommentForm commentForm) {
 		
+		var comment = new Comment(commentForm.getPostId() , commentForm.getUserId(), commentForm.getCommentContent(),);
 		return null;
 	}
 
 	@Override
 	public CommentView updateComment(CommentForm commentForm) {
+		var comment = commentModel.findOne(commentForm.getUserId());
+		if(comment == null) {
+			return null;
+		}
 		
+		var updatedComment = comment.updatedClone(LocalDateTime.now());
+		
+		updatedComment = new Comment
 		return null;
 	}
 
 	@Override
 	public boolean deleteComment(int commentId) {
 		
-		return false;
+        Comment comment = commentModel.findOne(commentId);
+        if (comment == null) {
+            return false;
+        }
+        return commentModel.delete(commentId);
 	}
 
 	@Override
 	public List<CommentView> getCommentViews(int postId, int lastCommentId, int i) {
-		
+		var comments = commentModel.find
 		return null;
 	}
 
