334aed8d25f34141fc2e3896ba277d0c49e66b19
Skyllar1 <skyllar64@gmail.com>
2024-08-24 23:46:54 +0630

    test

---
+++ social-app/src/main/java/edu/ucsy/social/service/impl/PostServiceImpl.java
@@ -0,0 +1,37 @@
+package edu.ucsy.social.service.impl;
+
+import java.util.List;
+
+import edu.ucsy.social.model.dto.view.PostDetailView;
+import edu.ucsy.social.model.dto.view.PostView;
+import edu.ucsy.social.service.PostService;
+
+public class PostServiceImpl implements PostService{
+	
+	
+
+	@Override
+	public List<PostView> getPostViews(int userid, int limit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PostView> getRandomPostViews(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PostDetailView getPostDetailView(int postId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletePost(int postId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
