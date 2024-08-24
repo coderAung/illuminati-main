d1b7aa216f76334cb500ad6b7a538f508496f2a9
Skyllar1 <skyllar64@gmail.com>
2024-08-24 22:03:49 +0630

    Merge branch 'service-impl-kph' of https://github.com/coderAung/illuminati-main.git into service-impl-kph

--- social-app/src/main/java/edu/ucsy/social/service/impl/CommentServiceImpl.java
+++ social-app/src/main/java/edu/ucsy/social/service/impl/CommentServiceImpl.java
@@ -3,26 +3,19 @@ package edu.ucsy.social.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
 import edu.ucsy.social.data.Model;
 import edu.ucsy.social.data.ModelFactory;
-import edu.ucsy.social.data.OneToOne;
-import edu.ucsy.social.data.db.ConnectorFactory;
 import edu.ucsy.social.model.dto.form.CommentForm;
 import edu.ucsy.social.model.dto.view.CommentView;
-import edu.ucsy.social.model.dto.view.ProfileView;
 import edu.ucsy.social.model.entity.Comment;
-import edu.ucsy.social.model.entity.CoverImage;
-import edu.ucsy.social.model.entity.ProfileImage;
 import edu.ucsy.social.service.CommentService;
 
 public class CommentServiceImpl implements CommentService {
 
 	private Model<Comment> commentModel;
 
-	public CommentServiceImpl(DataSource dataSource) {
-		this.commentModel = ModelFactory.getModel(Comment.class, ConnectorFactory.getConnector(dataSource));
+	public CommentServiceImpl() {
+		this.commentModel = ModelFactory.getModel(Comment.class);
 	}
 
 	@Override
@@ -53,7 +46,9 @@ public class CommentServiceImpl implements CommentService {
 			return null;
 		}
 
-		comment = new Comment(commentForm.getCommentContent(), commentForm.getUserId(), comment.userName(),
+		comment = new Comment(commentForm.getCommentContent(), 
+				commentForm.getUserId(), 
+				comment.userName(),
 				commentForm.getPostId());
 		;
 
