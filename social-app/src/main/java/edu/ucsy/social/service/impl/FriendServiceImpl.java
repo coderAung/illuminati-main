da8a62678b732256e0dd139c8e54f3a392b40481
Skyllar1 <skyllar64@gmail.com>
2024-08-24 22:23:22 +0630

    test

--- social-app/src/main/java/edu/ucsy/social/service/impl/FriendServiceImpl.java
+++ social-app/src/main/java/edu/ucsy/social/service/impl/FriendServiceImpl.java
@@ -2,14 +2,13 @@ package edu.ucsy.social.service.impl;
 
 import java.util.List;
 
-import edu.ucsy.social.data.Model;
 import edu.ucsy.social.model.dto.view.FriendView;
-import edu.ucsy.social.model.entity.Friend;
 import edu.ucsy.social.service.FriendService;
 
 public class FriendServiceImpl implements FriendService{
 
-	private Model<Friend> friendModel;
+	
+	
 	
 	@Override
 	public List<FriendView> getFriendViews(int userid, int limit) {
